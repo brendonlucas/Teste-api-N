fceb74877da502f9ed5abb225d094e058d2da3c8
brendonlucas <brendonplay007@gmail.com>
2022-10-29 20:46:22 -0300

    first commit

---
+++ api/urls.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from django.template.defaulttags import url
+from django.urls import path, include
+from rest_framework.authtoken.views import obtain_auth_token
+
+from usuario.views import CustomAuthToken
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('usuario.urls')),
+    #path('api-token-auth/', CustomAuthToken.as_view()),
+    path('api-token-auth/', obtain_auth_token),
+    path('api-token-auth2/', CustomAuthToken.as_view()),
+
+]
